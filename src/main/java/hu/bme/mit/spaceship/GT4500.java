0c7e833ac0fa36495108b3b6119cb8c78507b2a0
Daniil-BSD <daniil.bsd@gmail.com>
2020-04-09 11:39:21 +0200

    Commit A

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,7 +78,7 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
-          // try to fire individually
+          // try to fire individually.
           if (! primaryTorpedoStore.isEmpty()) {
             firingSuccess |= primaryTorpedoStore.fire(1);
           }
